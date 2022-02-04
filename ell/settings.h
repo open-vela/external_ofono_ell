60db27d48fd2072536c3f8c9111e5ea56a34bd0b
James Prestwood <prestwoj@gmail.com>
2022-02-04 09:46:10 -0800

    settings: add l_settings_clone
    Returns identical copy of settings

--- ell/settings.h
+++ ell/settings.h
@@ -37,6 +37,8 @@ typedef void (*l_settings_debug_cb_t) (const char *str, void *user_data);
 typedef void (*l_settings_destroy_cb_t) (void *user_data);
 
 struct l_settings *l_settings_new(void);
+struct l_settings *l_settings_clone(const struct l_settings *settings);
+
 void l_settings_free(struct l_settings *settings);
 DEFINE_CLEANUP_FUNC(l_settings_free);
 
