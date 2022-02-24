bryanperris/ell

commit 9843abcd84bc7bece45dd8e3b83a01ff246b7a71

utf8: cast to avoid left shift of negative value (ubsan)

ell/utf8.c
@@ -98,7 +98,7 @@ LIB_EXPORT int l_utf8_get_codepoint(const char *str, size_t len, wchar_t *cp)
 		return 1;
 	}
 
-	expect_bytes = __builtin_clz(~(str[0] << 24));
+	expect_bytes = __builtin_clz(~((unsigned char)str[0] << 24));
 
 	if (expect_bytes < 2 || expect_bytes > 4)
 		goto error;

open-vela/external_ofono_ell

commit 40a79e979a6fa5197192301e16ff4dd4c878cacd

utf8: fixed bit shift larger than type size
The C standard requires the bit shift result value width to be greater
than or equal to the number of bits being shifted, otherwise the behavior
is undefined. In this case str[0] was being cast to an unsigned char
(1 byte) but being shifted 24 bits. This resulted, on some compilers,
with a runtime error:

runtime error: left shift of 255 by 24 places cannot be represented in type 'int'

Casting str[0] to an unsigned int satisfies the width requirements
for this bit shift.

Fixes: 9843abcd84bc ("utf8: cast to avoid left shift of negative value (ubsan)")

ell/utf8.c
@@ -98,7 +98,7 @@ LIB_EXPORT int l_utf8_get_codepoint(const char *str, size_t len, wchar_t *cp)
 		return 1;
 	}
 
-	expect_bytes = __builtin_clz(~((unsigned char)str[0] << 24));
+	expect_bytes = __builtin_clz(~((unsigned int)str[0] << 24));
 
 	if (expect_bytes < 2 || expect_bytes > 4)
 		goto error;