open-vela/external_ofono_ell

commit 64238ccf471a951d48f4af322da52f55da604577

genl,netlink: Move extended ACK code to netlink-private
Since extended ACK parsing can be also useful for rtnl, move it from
genl.c to netlink.c and declare in netlink-private.h.

ell/genl.c
@@ -33,9 +33,9 @@
 #include "log.h"
 #include "queue.h"
 #include "io.h"
+#include "private.h"
 #include "netlink-private.h"
 #include "genl.h"
-#include "private.h"
 
 #define MAX_NESTING_LEVEL 4
 #define GENL_DEBUG(fmt, args...)	\
@@ -722,17 +722,6 @@ static bool match_request_hid(const void *a, const void *b)
 	return request->handle_id == id;
 }
 
-#define NLA_OK(nla,len)         ((len) >= (int) sizeof(struct nlattr) && \
-				(nla)->nla_len >= sizeof(struct nlattr) && \
-				(nla)->nla_len <= (len))
-#define NLA_NEXT(nla,attrlen)	((attrlen) -= NLMSG_ALIGN((nla)->nla_len), \
-				(struct nlattr*)(((char*)(nla)) + \
-				NLMSG_ALIGN((nla)->nla_len)))
-
-#define NLA_LENGTH(len)		(NLMSG_ALIGN(sizeof(struct nlattr)) + (len))
-#define NLA_DATA(nla)		((void*)(((char*)(nla)) + NLA_LENGTH(0)))
-#define NLA_PAYLOAD(nla)	((int)((nla)->nla_len) - NLA_LENGTH(0))
-
 static struct l_genl_msg *msg_alloc(uint8_t cmd, uint8_t version, uint32_t size)
 {
 	struct l_genl_msg *msg;
@@ -774,52 +763,13 @@ static bool msg_grow(struct l_genl_msg *msg, uint32_t needed)
 static struct l_genl_msg *msg_create(const struct nlmsghdr *nlmsg)
 {
 	struct l_genl_msg *msg;
+	const char *error_msg = NULL;
 
 	msg = l_new(struct l_genl_msg, 1);
 
-	if (nlmsg->nlmsg_type == NLMSG_ERROR) {
-		struct nlmsgerr *err = NLMSG_DATA(nlmsg);
-		unsigned int offset = 0;
-		struct nlattr *nla;
-		int len;
-
-		msg->error = err->error;
-
-		if (!(nlmsg->nlmsg_flags & NLM_F_ACK_TLVS))
-			goto done;
-
-		/*
-		 * If the message is capped, then err->msg.nlmsg_len contains
-		 * the length of the original message and thus can't be used
-		 * to calculate the offset
-		 */
-		if (!(nlmsg->nlmsg_flags & NLM_F_CAPPED))
-			offset = err->msg.nlmsg_len - sizeof(struct nlmsghdr);
-
-		/*
-		 * Attributes start past struct nlmsgerr.  The offset is 0
-		 * for NLM_F_CAPPED messages.  Otherwise the original message
-		 * is included, and thus the offset takes err->msg.nlmsg_len
-		 * into account
-		 */
-		nla = (void *)(err + 1) + offset;
-
-		/* Calculate bytes taken up by header + nlmsgerr contents */
-		offset += sizeof(struct nlmsghdr) + sizeof(struct nlmsgerr);
-		if (nlmsg->nlmsg_len <= offset)
-			goto done;
-
-		len = nlmsg->nlmsg_len - offset;
-
-		for (; NLA_OK(nla, len); nla = NLA_NEXT(nla, len)) {
-			if ((nla->nla_type & NLA_TYPE_MASK) !=
-					NLMSGERR_ATTR_MSG)
-				continue;
-
-			msg->error_msg = l_strdup(NLA_DATA(nla));
-			goto done;
-		}
-	}
+	if (netlink_parse_ext_ack(nlmsg, &error_msg, NULL) &&
+			error_msg)
+		msg->error_msg = l_strdup(error_msg);
 
 	msg->data = l_memdup(nlmsg, nlmsg->nlmsg_len);
 
@@ -833,7 +783,6 @@ static struct l_genl_msg *msg_create(const struct nlmsghdr *nlmsg)
 		msg->version = genlmsg->version;
 	}
 
-done:
 	return l_genl_msg_ref(msg);
 }
 

ell/netlink-private.h
@@ -23,3 +23,18 @@
 #ifndef SOL_NETLINK
 #define SOL_NETLINK 270
 #endif
+
+#define NLA_OK(nla,len)         ((len) >= (int) sizeof(struct nlattr) && \
+				(nla)->nla_len >= sizeof(struct nlattr) && \
+				(nla)->nla_len <= (len))
+#define NLA_NEXT(nla,attrlen)	((attrlen) -= NLMSG_ALIGN((nla)->nla_len), \
+				(struct nlattr*)(((char*)(nla)) + \
+				NLMSG_ALIGN((nla)->nla_len)))
+
+#define NLA_LENGTH(len)		(NLMSG_ALIGN(sizeof(struct nlattr)) + (len))
+#define NLA_DATA(nla)		((void*)(((char*)(nla)) + NLA_LENGTH(0)))
+#define NLA_PAYLOAD(nla)	((int)((nla)->nla_len) - NLA_LENGTH(0))
+
+bool netlink_parse_ext_ack(const struct nlmsghdr *nlmsg,
+				const char **out_error_msg,
+				uint32_t *out_error_offset);

ell/netlink.c
@@ -32,9 +32,9 @@
 #include "hashmap.h"
 #include "queue.h"
 #include "io.h"
+#include "private.h"
 #include "netlink-private.h"
 #include "netlink.h"
-#include "private.h"
 
 struct command {
 	unsigned int id;
@@ -608,6 +608,60 @@ LIB_EXPORT bool l_netlink_set_debug(struct l_netlink *netlink,
 	netlink->debug_data = user_data;
 
 	/* l_io_set_debug(netlink->io, function, user_data, NULL); */
+
+	return true;
+}
+
+bool netlink_parse_ext_ack(const struct nlmsghdr *nlmsg,
+				const char **out_error_msg,
+				uint32_t *out_error_offset)
+{
+	const struct nlmsgerr *err;
+	unsigned int offset = 0;
+	struct nlattr *nla;
+	int len;
+
+	if (nlmsg->nlmsg_type != NLMSG_ERROR ||
+			!(nlmsg->nlmsg_flags & NLM_F_ACK_TLVS))
+		return false;
+
+	err = NLMSG_DATA(nlmsg);
+
+	/*
+	 * If the message is capped, then err->msg.nlmsg_len contains the
+	 * length of the original message and thus can't be used to
+	 * calculate the offset.
+	 */
+	if (!(nlmsg->nlmsg_flags & NLM_F_CAPPED))
+		offset = err->msg.nlmsg_len - sizeof(struct nlmsghdr);
+
+	/*
+	 * Attributes start past struct nlmsgerr.  The offset is 0 for
+	 * NLM_F_CAPPED messages.  Otherwise the original message is
+	 * included, and thus the offset takes err->msg.nlmsg_len into
+	 * account.
+	 */
+	nla = (void *)(err + 1) + offset;
+
+	/* Calculate bytes taken up by header + nlmsgerr contents */
+	offset += sizeof(struct nlmsghdr) + sizeof(struct nlmsgerr);
+	if (nlmsg->nlmsg_len <= offset)
+		return false;
+
+	len = nlmsg->nlmsg_len - offset;
+
+	for (; NLA_OK(nla, len); nla = NLA_NEXT(nla, len)) {
+		switch (nla->nla_type & NLA_TYPE_MASK) {
+		case NLMSGERR_ATTR_MSG:
+			if (out_error_msg)
+				*out_error_msg = NLA_DATA(nla);
+			break;
+		case NLMSGERR_ATTR_OFFS:
+			if (out_error_offset)
+				*out_error_offset = l_get_u32(NLA_DATA(nla));
+			break;
+		}
+	}
 
 	return true;
 }

commit 6e8ccbd285ccb8f86d374eb97c022f7c75f1433a

genl: Restore handling of errors
Error handling for genl was broken.  Fix that.

Also while here, rename netlink_parse_ext_ack() to
netlink_parse_ext_ack_error() so as to better convey it intended
purpose.  Drop certain checks from netlink_parse_ext_ack_error() since
they were redundant.

Fixes: 64238ccf471a ("genl,netlink: Move extended ACK code to netlink-private")

ell/genl.c
@@ -763,13 +763,21 @@ static bool msg_grow(struct l_genl_msg *msg, uint32_t needed)
 static struct l_genl_msg *msg_create(const struct nlmsghdr *nlmsg)
 {
 	struct l_genl_msg *msg;
-	const char *error_msg = NULL;
 
 	msg = l_new(struct l_genl_msg, 1);
 
-	if (netlink_parse_ext_ack(nlmsg, &error_msg, NULL) &&
-			error_msg)
-		msg->error_msg = l_strdup(error_msg);
+	if (nlmsg->nlmsg_type == NLMSG_ERROR) {
+		struct nlmsgerr *err = NLMSG_DATA(nlmsg);
+		const char *error_msg = NULL;
+
+		msg->error = err->error;
+
+		if (netlink_parse_ext_ack_error(nlmsg, &error_msg, NULL) &&
+						error_msg)
+			msg->error_msg = l_strdup(error_msg);
+
+		goto done;
+	}
 
 	msg->data = l_memdup(nlmsg, nlmsg->nlmsg_len);
 
@@ -783,6 +791,7 @@ static struct l_genl_msg *msg_create(const struct nlmsghdr *nlmsg)
 		msg->version = genlmsg->version;
 	}
 
+done:
 	return l_genl_msg_ref(msg);
 }
 

ell/netlink-private.h
@@ -35,6 +35,6 @@
 #define NLA_DATA(nla)		((void*)(((char*)(nla)) + NLA_LENGTH(0)))
 #define NLA_PAYLOAD(nla)	((int)((nla)->nla_len) - NLA_LENGTH(0))
 
-bool netlink_parse_ext_ack(const struct nlmsghdr *nlmsg,
-				const char **out_error_msg,
-				uint32_t *out_error_offset);
+bool netlink_parse_ext_ack_error(const struct nlmsghdr *nlmsg,
+					const char **out_error_msg,
+					uint32_t *out_error_offset);

ell/netlink.c
@@ -165,8 +165,10 @@ static void process_ext_ack(struct l_netlink *netlink,
 	uint32_t err_offset = -1U;
 	_auto_(l_free) char *dbg_str = NULL;
 
-	if (!netlink->debug_handler ||
-			!netlink_parse_ext_ack(nlmsg, &err_str, &err_offset) ||
+	if (!netlink->debug_handler)
+		return;
+
+	if (!netlink_parse_ext_ack_error(nlmsg, &err_str, &err_offset) ||
 			(!err_str && err_offset == -1U))
 		return;
 
@@ -646,20 +648,21 @@ LIB_EXPORT bool l_netlink_set_debug(struct l_netlink *netlink,
 	return true;
 }
 
-bool netlink_parse_ext_ack(const struct nlmsghdr *nlmsg,
-				const char **out_error_msg,
-				uint32_t *out_error_offset)
+/*
+ * Parses extended error info from the extended ack.  It is assumed that the
+ * caller has already checked the type of @nlmsg and it is of type NLMSG_ERROR.
+ */
+bool netlink_parse_ext_ack_error(const struct nlmsghdr *nlmsg,
+					const char **out_error_msg,
+					uint32_t *out_error_offset)
 {
-	const struct nlmsgerr *err;
+	const struct nlmsgerr *err = NLMSG_DATA(nlmsg);
 	unsigned int offset = 0;
 	struct nlattr *nla;
 	int len;
 
-	if (nlmsg->nlmsg_type != NLMSG_ERROR ||
-			!(nlmsg->nlmsg_flags & NLM_F_ACK_TLVS))
+	if (!(nlmsg->nlmsg_flags & NLM_F_ACK_TLVS))
 		return false;
-
-	err = NLMSG_DATA(nlmsg);
 
 	/*
 	 * If the message is capped, then err->msg.nlmsg_len contains the