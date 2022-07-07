49fa626ce1776492d85d11e1af80859ae035d075
Denis Kenzior <denkenz@gmail.com>
2022-07-07 14:39:55 -0500

    key: ECDSA data is given in x962 format
    When using the verify operation with ECDSA based public keys, the new
    format type must be used.

--- ell/key.h
+++ ell/key.h
@@ -56,6 +56,7 @@ enum l_keyring_restriction {
 enum l_key_cipher_type {
 	L_KEY_RSA_PKCS1_V1_5,
 	L_KEY_RSA_RAW,
+	L_KEY_ECDSA_X962,
 };
 
 struct l_key *l_key_new(enum l_key_type type, const void *payload,
